b7a506321de85fb2d35bd32471a4291c1a18b133
Bailey Chittle <baileyc@tessonics.com>
2023-03-10 15:27:01 -0500

    rest api hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bachittle/self-hosted-chatgpt
 
 go 1.20
+
+require github.com/alecthomas/kong v0.7.1
--- main.go
+++ main.go
@@ -2,6 +2,42 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/alecthomas/kong"
+)
+
+var cli struct {
+	SetKey string          `help:"Set OpenAI API key."`
+	Run    RunWebServerCmd `cmd:"" help:"Run the web server."`
+}
+
+const APP_VERSION = "0.0.1"
+
+func main() {
+	ctx := kong.Parse(&cli,
+		kong.Name("openai-server"),
+		kong.Description("One-file web server interfacing with openai api."),
+		kong.UsageOnError(),
+		kong.Vars{"version": APP_VERSION},
+	)
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
+}
+
+type RunWebServerCmd struct {
+	Port int `help:"Port to listen on." default:"8080"`
+}
+
+func (r *RunWebServerCmd) Run() error {
+	fmt.Println("Running web server on port", r.Port)
+
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	http.ListenAndServe(fmt.Sprintf(":%d", r.Port), nil)
+
+	return nil
 }
