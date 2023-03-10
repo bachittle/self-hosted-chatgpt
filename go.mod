1263c08d7dc9d4f57827f65dc477ef0bd21f79e7
Bailey Chittle <bachittle@gmail.com>
2023-03-10 18:13:46 -0500

    reading/writing secret key from env var

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bachittle/self-hosted-chatgpt
 
 go 1.20
 
-require github.com/alecthomas/kong v0.7.1
+require (
+	github.com/alecthomas/kong v0.7.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"net/http"
 
 	"github.com/alecthomas/kong"
+
+	"github.com/joho/godotenv"
 )
 
 var cli struct {
-	SetKey string          `help:"Set OpenAI API key."`
-	Run    RunWebServerCmd `cmd:"" help:"Run the web server."`
+	Key KeyCmd          `cmd:"" help:"Either set OpenAI API key, or get the key that is saved."`
+	Run RunWebServerCmd `cmd:"" help:"Run the web server."`
 }
 
 const APP_VERSION = "0.0.1"
@@ -25,6 +27,48 @@ func main() {
 	)
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
+}
+
+type KeyCmd struct {
+	Value string `arg:"" optional:"" help:"Override the OpenAI API key with this value."`
+}
+
+func (s *KeyCmd) Run() error {
+	if s.Value == "" {
+		fmt.Println("getting the env var from .env file...")
+		val, err := getApiKey()
+		if err != nil {
+			fmt.Println("Error getting OpenAI API key: ", err)
+			return err
+		}
+		s.Value = val
+
+	} else {
+		fmt.Println("setting the env var using value given...")
+		err := setApiKey(s.Value)
+		if err != nil {
+			fmt.Println("Error setting OpenAI API key: ", err)
+			return err
+		}
+	}
+	fmt.Println("OpenAI API key: ", s.Value)
+	return nil
+}
+
+func getApiKey() (string, error) {
+	env, err := godotenv.Read(".env")
+	if err != nil {
+		return "", err
+	}
+	return env["OPENAI_API_KEY"], nil
+}
+
+func setApiKey(val string) error {
+	err := godotenv.Write(map[string]string{"OPENAI_API_KEY": val}, ".env")
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type RunWebServerCmd struct {
