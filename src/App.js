ba4aff61f20dfd4358a0ea0b265e314fb01085ae
Bailey Chittle <bachittle@gmail.com>
2023-03-11 00:48:19 -0500

    better rendering of chat messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { useState } from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 
 function App() {
@@ -8,7 +9,12 @@ function App() {
 
   const handleChat = (event) => {
     event.preventDefault();
+
+    if (event.target.elements[0].value === "") {
+      return;
+    }
     const message = event.target.elements[0].value;
+    event.target.elements[0].value = "";
     setMsgs([...msgs, {role: "user", msg: message}])
 
     axios.post('http://localhost:8080/api/chat', {
@@ -25,11 +31,21 @@ function App() {
   return (
     <div className="App">
       <h1>Self-hosted Chat GPT</h1>
-      <ul>
+      <div>
         {msgs.map((msg, index) => {
-          return <li key={index}>{msg.role}: {msg.msg}</li>
+          if (msg.role === "user") {
+            return (
+              <div key={index} style={{backgroundColor: "#FEE"}}>{msg.msg}</div>
+            )
+          } else {
+            return (
+              <div key={index} style={{backgroundColor: "#EEF"}}>
+                <ReactMarkdown>{msg.msg}</ReactMarkdown>
+              </div>
+            )
+          }
         })}
-      </ul>
+      </div>
       <Form onSubmit={handleChat}>
         <Form.Group>
           <Form.Label>Message</Form.Label>
